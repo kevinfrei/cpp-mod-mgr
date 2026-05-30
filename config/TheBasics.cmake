92a3a6f17296315d65a5bd30cb59c3202b123c61
Kevin Frei <kevinfrei@hotmail.com>
2026-05-30 16:06:34 -0700

    MSVC now works...

--- config/TheBasics.cmake
+++ config/TheBasics.cmake
@@ -1,6 +1,7 @@
 # C++23, cuz we're living in the future
 set(CMAKE_CXX_STANDARD 23)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
+
 # Extensions are a way to break your app on different OS'es. Bad bad bad
 set(CMAKE_CXX_EXTENSIONS OFF)
 
@@ -12,22 +13,38 @@ if(MSVC)
   # And for the love of god, turn of the CRT security warnings.
   # Having deployed those things across all of Microsoft in 2005, let's
   # just say "LKG666" wasn't very fun.
-  add_compile_options(/EHsc /W4)
   add_compile_definitions(
     _WIN32_WINNT=0x0A00
     WINVER=0x0A00
     _CRT_SECURE_NO_WARNINGS
   )
+  add_compile_options(/EHsc /W4 /std:c++latest)
   if(CMAKE_BUILD_TYPE STREQUAL "Debug")
+    # Let's try STL as a module. This is so abusive to users. Why can't the
+    # compiler actually build the .ifc/.pcm file as needed and cache the thing?
+    # Oh, because compiler people think they're the smartest engineers on the
+    # planet, but can't figure out how to make a !@#$ing file cache, so they
+    # define the problem away as as 'build configuration'.
+    #
+    # Narrator: Kevin was, in fact, also a compiler person...
+    add_compile_options(
+      /reference
+      "std=$ENV{VCToolsInstallDir}modules\\MTd\\std.ifc"
+    )
     # Hey, look, a feature I worked on!
     set(CMAKE_MSVC_DEBUG_INFORMATION_FORMAT EditAndContinue)
     set(CMAKE_MSVC_RUNTIME_LIBRARY MultiThreadedDebug)
     # Hey look, I did this work, too!
     set(CMAKE_MSVC_RUNTIME_CHECKS "StackFrameErrorCheck;UninitializedVariable")
-    # Explicitly debug build, and turn on mspdbsrv.exe, cuz multiproc builds are fast
+    # Explicitly debug build, and turn on mspdbsrv.exe,
+    # cuz multiproc builds are fast
     add_compile_options(/Od /ZI /FS)
     add_link_options(/DEBUG:FULL /PROFILE)
   else()
+    add_compile_options(
+      /reference
+      "std=$ENV{VCToolsInstallDir}modules\\MT\\std.ifc"
+    )
     set(CMAKE_MSVC_DEBUG_INFORMATION_FORMAT ProgramDatabase)
     set(CMAKE_MSVC_RUNTIME_LIBRARY MultiThreaded)
 
---
+++ import-std-on-msvc.md
@@ -0,0 +1,28 @@
+# Getting 'import std;' working with Visual C++ & CMake
+
+I'm probably weird, but I find the idea of needing set a !@#$ing GUID in my
+build settings to enable CMake's experimental support for `import std;`
+off-putting.
+
+So, instead, I'm building stuff myself, because that's what I apparently do.
+
+Here's what I did to create the `.ifc` and `.obj` files that MSVC wants in order
+to properly using `import std;` from random C++ land.:
+
+(I did all this as administrator, because I'm writing to a protected location)
+
+```sh
+cd %VCToolsInstallDir%
+mkdir MT MTd MD MDd
+cl /std:c++latest /EHsc /nologo /MT /DNDEBUG /c std.ixx /ifcOutput MT\ /FoMT\
+cl /std:c++latest /EHsc /nologo /MTd /D_DEBUG /c std.ixx /ifcOutput MTd\ /FoMTd\
+cl /std:c++latest /EHsc /nologo /MD /DNDEBUG /c std.ixx /ifcOutput MD\ /FoMD\
+cl /std:c++latest /EHsc /nologo /MDd /D_DEBUG /c std.ixx /ifcOutput MDd\ /FoMDd\
+```
+
+That basically builds the 4 'common' configurations of the standard library:
+Static Lib/Static Debug Lib, and DLL, Debug DLL.
+
+Then you have to add
+`/reference "std=$ENV{VCToolsInstallDir}modules\\MTd\\std.ifc"` as a compile
+options (replace MTd with your C++ preferred runtime flavor).
